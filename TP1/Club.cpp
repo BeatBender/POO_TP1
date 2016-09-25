57634f392d81dab30d428f678fd7f5f4dd1d4540
Patrick Levesque <levesque_patrick_2@hotmail.com>
2016-09-24 21:05:48 -0400

    Euh... commencement, j'suis dans marde

--- TP1/Club.cpp
+++ TP1/Club.cpp
@@ -1,11 +1,42 @@
 #include "Club.h"
+#include <iostream>
 
 Club::Club()
 {
-
+	nom = new std::string;
+	histoire = new std::string;
+	couleur = new std::string;
+	ville = new std::string;
+	annee_creation = new Date();
+	palmares = new Palmares;
+	stade_du_club = new Stade;
 }
 
 Club::~Club()
 {
+	delete nom;
+	delete histoire;
+	delete couleur;
+	delete ville;
+	delete annee_creation;
+	delete palmares;
+}
 
+void Club::SetClub(Club*)
+{
+	std::cin >> *palmares;
+	printf("Veuillez entrer le nom du club: ");
+	scanf("%s", *nom);
+	printf("Entrez la ville du club: ");
+	scanf("%s", *ville);
+	printf("Entrez la couleur du club: ");
+	scanf("%s", *couleur);
+	printf("Entrez l'histoire du club: ");
+	scanf("%s", *histoire);
+	printf("Entrez la date de creation du club: ");
+	scanf("%Date", *annee_creation);
+	printf("Entrez un palmares du club: ");
+	scanf("%Palmares", *palmares);
+	printf("Entrez le stade su club: ");
+	scanf("%Stade", stade_du_club);
 }
--- TP1/Club.h
+++ TP1/Club.h
@@ -10,13 +10,14 @@ class Club
 {
 public:
 	Club();
+	void SetClub(Club*);
 	~Club();
 
 private:
-	std::string histoire, couleur, ville;
-	Date annee_creation;
-	std::list<Joueur> effectif;
-	Palmares palmares;
-	Stade stade_du_club;
-	std::list<Personne> staff;
+	std::string *histoire, *couleur, *ville, *nom;
+	Date* annee_creation;
+	std::list<Joueur>* effectif;
+	Palmares* palmares;
+	Stade* stade_du_club;
+	std::list<Personne>* staff;
 };
